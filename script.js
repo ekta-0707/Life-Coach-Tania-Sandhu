b686a852ab9d5a67082e5da40174ebf1cf00f27f
ekta-0707 <ekta67649@gmail.com>
2025-06-11 19:26:32 -0600

    changes

--- script.js
+++
@@ -1,9 +0,0 @@
-// js/scripts.js
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#contact-form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Validate and process form data
-    alert('Form submitted!');
-  });
-});
